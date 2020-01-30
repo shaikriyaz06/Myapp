9d1d1f3ad63c58d64134a0f956c31205379afb4a
shaikriyaz06 <shaikriyaz222@gmail.com>
2020-01-30 09:47:12 +0530

    build

---
+++ build/precache-manifest.925bca825b7b37f0f16f19602931ccfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6d6f7c305edf046881e669bac477c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "908ad6798dea49d19f45",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "23a15c2e093a18c7520c",
+    "url": "/static/js/2.15f7a68f.chunk.js"
+  },
+  {
+    "revision": "908ad6798dea49d19f45",
+    "url": "/static/js/main.e886480b.chunk.js"
+  },
+  {
+    "revision": "de463be5d5aa0ad86ed5",
+    "url": "/static/js/runtime~main.d37a8383.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
